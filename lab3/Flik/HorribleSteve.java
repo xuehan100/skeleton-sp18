fa131c93ef2339916c9ccbb5fe2ef8302ec6ebb8
Xuehan Cheng <xuehan868@gmail.com>
2020-02-01 00:54:48 -0800

    Complete lab3

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,9 +1,10 @@
 public class HorribleSteve {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws Exception {
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
-                break; // break exits the for loop!
+                //break; // break exits the for loop!
+                throw new Exception(String.format("i:%d not same as j:%d ??", i, j));
             }
         }
         System.out.println("i is " + i);
